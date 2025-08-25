bf8a45aa088d968ce4abedcc0198e8029ac1e711
anubhavgupta76685 <anubhavgupta76685@gmail.com>
2025-08-25 14:51:14 +0530

    Update divisible_by_3and_last_digit_is4.py

--- Sheet 1/divisible_by_3and_last_digit_is4.py
+++ Sheet 1/divisible_by_3and_last_digit_is4.py
@@ -4,4 +4,4 @@ n = int(input("Enter a number: "))
 if n % 3 == 0 and n % 10 == 4:
     print("The number is divisible by 3 and the last digit is 4")
 else:
-    print("condition is not satisfy")
+    print("invalid")
